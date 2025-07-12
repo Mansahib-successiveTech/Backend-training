fd80d4a44f5157d7a6264f7fe85e21967c3db344
Mansahib Singh <mansahib.singh@mansahibvrs16.successivetech.com>
2025-07-12 15:09:32 +0530

    updated less complex nodemon setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from "url";
-import { add, sub, mul, div } from "./lib/math";
+import { add, sub, mul, div } from "./lib/math.js";
 
 // Required for __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url);
