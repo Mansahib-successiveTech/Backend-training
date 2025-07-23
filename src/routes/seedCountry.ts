9d6aa9d91195cd3279f43bf8f609eaadefc3eea8
Mansahib Singh <mansahib.singh@mansahibvrs16.successivetech.com>
2025-07-23 14:44:05 +0530

    features/assignment-8 update folder structure

--- src/routes/seedCountry.ts
+++ src/routes/seedCountry.ts
@@ -1,6 +1,6 @@
 import express from "express"
-import { States } from "../services/stateData";
-import { CountrySeed } from "../services/countrySeed";
+import { States } from "../controller/stateData";
+import { CountrySeed } from "../controller/countrySeed";
 
 const countryRoute=express.Router();
 const state=new States();
